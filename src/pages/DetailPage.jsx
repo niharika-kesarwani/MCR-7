5bb7ad4dc45208ec6fd6155d8482a429d09fc315
NiharikaNeha <kesarwani.neha9@gmail.com>
2023-07-09 22:05:40 +0530

    style: minor fix

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import { useTrip } from "../main";
 
 export const DetailPage = () => {
@@ -5,14 +6,21 @@ export const DetailPage = () => {
 
     const {id, name, description, image, ratings, reviews, location, openingHours, ticketPrice, website} = place;
 
-    return <div className="flex flex-col">
-        <div>{name}</div>
-        <div className="flex">
+    return <div className="min-h-screen flex flex-col items-center justify-center gap-5">
+        <div className="text-3xl font-bold">{name}</div>
+        <div className="flex flex-col md:flex-row gap-5 mx-10">
         <div>
             <img src={image}/>
         </div>
-        <div>
-            </div>
+        <div className="flex flex-col justify-between">
+            <div><span className="text-blue-600">Description: </span>{description}</div>
+            <div><span className="text-blue-600">Ratings: </span>{ratings}</div>
+            <div><span className="text-blue-600">Reviews: </span>{reviews}</div>
+            <div><span className="text-blue-600">Location: </span>{location}</div>
+            <div><span className="text-blue-600">Opening Hours: </span>{openingHours}</div>
+            <div><span className="text-blue-600">Ticket Price: </span>{ticketPrice}</div>
+            <NavLink to={website} target="_blank" className="text-pink-600 hover:cursor-pointer">Website</NavLink>
+        </div>
         </div>
        </div>
 }
